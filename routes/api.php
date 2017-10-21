03f9a50a6168ab62de4dcaf73286527060672d20
garam park <me@garam.xyz>
2017-10-22 06:16:27 +0900

    Rout 구현

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,81 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Models\Todo;
+
+Route::get('/todos', function () {
+	return Todo::orderBy('created_at', 'asc')->paginate();
+});
+
+Route::get('/todos/{id}', function ($id) {
+
+	$todo = Todo::find($id);
+
+	if($todo){
+		return $todo;	
+	}
+
+	return response()->json(['message' => 'Not found'],404);
+	
+});
+
+Route::post('/todos', function (Request $request) {
+	
+	$only = $request->only(['name']);
+
+	$validator = Validator::make($only, [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return response()->json(['errors' => $validator->errors()],400);
+	}
+
+	return Todo::create($only);
+
+});
+
+Route::put('/todos/{todo}', function (Request $request, $id) {
+
+	$only = $request->only(['name']);
+
+	$validator = Validator::make($only, [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return response()->json(['errors' => $validator->errors()],400);
+	}
+
+	$todo = Todo::find($id);
+	
+	if($todo){
+		
+		foreach ($only as $key => $value) {
+			$todo->$key = $value;
+		}
+
+		$todo->save();
+		
+		return $todo;
+	}
+
+	return response()->json(['message' => 'Not found'],404);
+	
+});
+
+/**
+ * Delete Todo
+ */
+Route::delete('/todos/{todo}', function ($id) {
+
+	$todo = Todo::find($id);
+	if($todo){
+		$todo->delete();
+		return $todo;
+	}
+
+	return response()->json(['message' => 'Not found'],404);
+	
+});
